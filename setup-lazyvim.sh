8f2bf2bad8c6ab0d0ddc0f69d74c8cde57a24f18
chayveng <33687510+chayveng@users.noreply.github.com>
2025-08-04 15:25:11 +0700

    Update setup-lazyvim.sh

--- setup-lazyvim.sh
+++ setup-lazyvim.sh
@@ -1,13 +1,10 @@
-#!/bin/bash
-
+## Lazyvim
 sudo apt install -y libfuse2
 wget https://github.com/neovim/neovim/releases/download/v0.10.0/nvim.appimage
-
 mv ~/.config/nvim{,.bak}
 mv ~/.local/share/nvim{,.bak}
 mv ~/.local/state/nvim{,.bak}
 mv ~/.cache/nvim{,.bak}
-
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 nvim
